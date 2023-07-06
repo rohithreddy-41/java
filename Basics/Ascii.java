847a9b0d7035e1de420a1449208dcf0a7636187c
rohithreddy-41 <vasudivinur123@gmail.com>
2023-07-06 13:04:19 +0530

    final commit

---
+++ Basics/Ascii.java
@@ -0,0 +1,12 @@
+
+public class Ascii {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 127; i++) {
+			// Convert the integer value to its corresponding character
+            char c = (char) i; 
+            
+            // Print the ASCII value and corresponding character
+            System.out.println("ASCII value: " + i + ", Character: " + c);
+        }
+    }
+}
