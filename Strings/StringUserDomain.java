e5990991307effbf7872aaa30cc1fd08674b98e3
rohithreddy-41 <vasudivinur123@gmail.com>
2023-07-09 10:31:26 +0530

    final commit

--- Strings/StringUserDomain.java
+++ Strings/StringUserDomain.java
@@ -1,23 +1,23 @@
 public class StringUserDomain {
 
     public static void main(String[] args) {
-        // Initializing the email address
+         // Initializing the email address
         String str = "programmer@gmail.com";
         
-        // Finding the index of the '@' symbol
+         // Finding the index of the '@' symbol
         int i = str.indexOf("@");
         
-        // Extracting the user part of the email address
+         // Extracting the user part of the email address
         String user = str.substring(0, i);
         
-        // Extracting the domain part of the email address
+         // Extracting the domain part of the email address
         String domain = str.substring(i + 1, str.length());
         
-        // Printing the user and domain parts
+         // Printing the user and domain parts
         System.out.println("user: " + user);
         System.out.println("domain: " + domain);
         
-        // Checking if the domain is equal to "gmail"
+         // Checking if the domain is equal to "gmail"
         int j = str.indexOf(".");
         String a = str.substring(i + 1, j);
         System.out.println(a);
