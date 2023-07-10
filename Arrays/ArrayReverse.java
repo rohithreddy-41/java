54cb30b7ff290da8c01199b2d4b5ca8b46abfc42
rohithreddy-41 <vasudivinur123@gmail.com>
2023-07-10 23:10:54 +0530

    first commit

---
+++ Arrays/ArrayReverse.java
@@ -0,0 +1,16 @@
+
+public class RevArray {
+
+   
+    public static void main(String[] args) {
+       
+       //reverse an array
+       
+        int a[]={2,3,4,5,6};
+        for(int i=a.length-1;i>=0;i--)
+        {
+        System.out.println(a[i]);
+        }
+    }
+    
+}
