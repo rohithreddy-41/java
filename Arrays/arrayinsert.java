78aba09532ca2af1d91cbff64251c71436fa4c23
rohithreddy-41 <vasudivinur123@gmail.com>
2023-07-10 23:17:39 +0530

    second commit

--- Arrays/arrayinsert.java
+++ Arrays/arrayinsert.java
@@ -1,24 +1,33 @@
+public class arrayinsert {
 
+    public static void main(String[] args) {
+        int a[] = new int[10]; // Initialize a new integer array with size 10
 
+        // Initialize some initial values in the array
+        a[0] = 2;
+        a[1] = 3;
+        a[2] = 5;
+        a[3] = 6;
+        a[4] = 7;
 
-public class arinsert {
-	
-	public static void main (String[] args) {
-		int a[]=new int[10];
-		a[0]=2;a[1]=3;a[2]=5;a[3]=6;a[4]=7;
-		int n=5;
-		int t=4;
-		int index=2;
-		for(int i=n;i>index;i--)
-		{
-			a[i]=a[i-1];
-			}
-			a[index]=t;
-			for(int i=0;i<=n;i++)
-			{
-				System.out.print(a[i]+",");
-				}
-		
-	}
+        // Current number of elements in the array
+        int n = 5; 
+         // Value to be inserted
+        int t = 4;
+         // Index at which the value should be inserted
+        int index = 2;
+
+        // Shift the elements to the right from index 'index' to make space for the new value
+        for (int i = n; i > index; i--) {
+            a[i] = a[i - 1];
+        }
+
+        // Insert the new value at the specified index
+        a[index] = t;
+
+        // Print the modified array
+        for (int i = 0; i <= n; i++) {
+            System.out.print(a[i] + ",");
+        }
+    }
 }
-
