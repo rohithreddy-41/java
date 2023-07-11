3a708e3417fa4f0a4caaa0f699370d2d07de00e9
rohithreddy-41 <vasudivinur123@gmail.com>
2023-07-11 22:06:22 +0530

    final commit

---
+++ Arrays/Array.java
@@ -0,0 +1,15 @@
+public class Array {
+    public static void main(String[] args) {
+       
+        // Print the elements of an array
+
+        // Declare and initialize the array
+        int a[] = {2,3,4,5,6,7,8};
+
+        // Iterate through the array
+        for (int i = 0; i < a.length; i++) {
+            // Print the element at the current index
+            System.out.println(a[i]);
+        }
+    }
+}
