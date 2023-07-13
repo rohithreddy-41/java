2f3a1be3117cd6d548d86dfba79a728d5ec939a4
rohithreddy-41 <vasudivinur123@gmail.com>
2023-07-13 15:43:14 +0530

    final commit

--- Arrays/arrayrevcopy.java
+++ Arrays/arrayrevcopy.java
@@ -1,23 +1,23 @@
-
-
-
 public class arrayrevcopy {
 	
 	public static void main (String[] args) {
+		// Initialize an array 'a' with values 1 to 10
 		int a[]={1,2,3,4,5,6,7,8,9,10};
+		
+		// Create a new array 'b' with the same size as 'a'
 		int b[]=new int[10];
+		
+		// Declare variables 'i' and 'j' for iteration
 		int i,j;
-		for(i=a.length-1,j=0;i>=0;i--,j++)
-		{
-			
+		
+		// Loop through the elements of 'a' in reverse order and copy them to 'b'
+		for(i=a.length-1,j=0;i>=0;i--,j++) {
 			b[j]=a[i];
 		}
-			for(int x:b)
-			{
-				System.out.print(x+",");
-				}
 		
-		
+		// Print the elements of array 'b'
+		for(int x:b) {
+			System.out.print(x+",");
+		}
 	}
 }
-
