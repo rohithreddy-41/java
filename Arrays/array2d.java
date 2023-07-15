24f036a440b55af31b425f4116588d5d0a57e832
rohithreddy-41 <vasudivinur123@gmail.com>
2023-07-15 22:33:30 +0530

    first commit

---
+++ Arrays/array2d.java
@@ -0,0 +1,18 @@
+
+
+public class array2d {
+	
+	public static void main (String[] args) {
+		int b[][]={{1,2,3,4},{5,6,7,8},{9,1,2,3}};
+		for(int i=0;i<b.length;i++)
+		{
+			for(int j=0;j<b[i].length;j++)
+			{
+				System.out.print(b[i][j]+" ");
+				}
+				System.out.println("");
+			}
+		
+	}
+}
+
