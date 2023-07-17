c192950fc544dcab4f6c5ac0eb6317f263905c98
rohithreddy-41 <vasudivinur123@gmail.com>
2023-07-17 13:55:23 +0530

    final commit

--- Basics/Randomnumbers.java
+++ Basics/Randomnumbers.java
@@ -7,7 +7,7 @@ public class Randomnumbers {
 
         // Generate and print 10 random integers between 0 and 100 (inclusive)
         for (int i = 0; i < 10; i++) {
-			// Generates a random integer between 0 and 100
+		// Generates a random integer between 0 and 100
             int randomNumber = random.nextInt(101); 
             System.out.println(randomNumber);
         }
