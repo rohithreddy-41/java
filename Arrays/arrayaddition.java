4e0be13c9ff1ab0fd0d739f1fa906a43371f5706
rohithreddy-41 <vasudivinur123@gmail.com>
2024-02-17 21:17:45 +0530

    Final commit

--- Arrays/arrayaddition.java
+++ Arrays/arrayaddition.java
@@ -1,11 +1,11 @@
 public class arrayaddition {
 	
 	public static void main (String[] args) {
-		int a[][]={{2,3,4},{1,2,3},{3,4,5}};
+		int a[][]={{2,3,4},{1,2,3},{3,4,9}};
 		int b[][]={{1,2,3},{3,4,5},{2,4,5}};
 		int c[][]=new int[3][3];
 		
-		// Iterate over the rows of the arrays 'a' and 'b'
+		 // Iterate over the rows of the arrays 'a' and 'b'
 		for(int i=0;i<a.length;i++)
 		{
 			// Iterate over the columns of the arrays 'a' and 'b'
